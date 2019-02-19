dc4055431dd8a65cf88b9b3b4bab5809b6c2fdc6
MrProger <47749473+MrProger@users.noreply.github.com>
2019-02-19 13:23:40 +0300

    Update 01-strings-tasks.js

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -20,7 +20,7 @@
  *   '',  'bb'  => 'bb'
  */
 function concatenateStrings(value1, value2) {
-  throw new Error('Not implemented');
+  return value1 + value2;
 }
 
 /**
@@ -35,7 +35,7 @@ function concatenateStrings(value1, value2) {
  *   ''      => 0
  */
 function getStringLength(value) {
-  throw new Error('Not implemented');
+  return value.length;
 }
 
 /**
@@ -52,7 +52,7 @@ function getStringLength(value) {
  *   'Chuck','Norris'  => 'Hello, Chuck Norris!'
  */
 function getStringFromTemplate(firstName, lastName) {
-  throw new Error('Not implemented');
+  return `Hello, ${firstName} ${lastName}!`;
 }
 
 /**
@@ -66,7 +66,7 @@ function getStringFromTemplate(firstName, lastName) {
  *   'Hello, Chuck Norris!' => 'Chuck Norris'
  */
 function  extractNameFromTemplate(value) {
-  throw new Error('Not implemented');
+  return value.slice(7, value.length);
 }
 
 
@@ -81,7 +81,7 @@ function  extractNameFromTemplate(value) {
  *   'cat'       => 'c'
  */
 function getFirstChar(value) {
-  throw new Error('Not implemented');
+  return value[0];
 }
 
 /**
@@ -96,7 +96,7 @@ function getFirstChar(value) {
  *   '\tHello, World! ' => 'Hello, World!'
  */
 function removeLeadingAndTrailingWhitespaces(value) {
-  throw new Error('Not implemented');
+   return value.trim();
 }
 
 /**
@@ -111,8 +111,9 @@ function removeLeadingAndTrailingWhitespaces(value) {
  *   'cat', 3 => 'catcatcat'
  */
 function repeatString(value, count) {
-  throw new Error('Not implemented');
+  return value.repeat(count);
 }
+
 
 /**
  * Remove the first occurrence of string inside another string
@@ -127,7 +128,7 @@ function repeatString(value, count) {
  *   'ABABAB','BA' => 'ABAB'
  */
 function removeFirstOccurrences(str, value)  {
-  throw new Error('Not implemented');
+  return str.replace(value, '');
 }
 
 /**
@@ -142,9 +143,8 @@ function removeFirstOccurrences(str, value)  {
  *   '<a>' => 'a'
  */
 function unbracketTag(str) {
-  throw new Error('Not implemented');
+  return str.replace(/[<>]/g, '');
 }
-
 
 /**
  * Converts all characters of the specified string into the upper case
@@ -157,9 +157,8 @@ function unbracketTag(str) {
  *  'abcdefghijklmnopqrstuvwxyz' => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
  */
 function convertToUpperCase(str) {
-  throw new Error('Not implemented');
+  return str.toUpperCase();
 }
-
 /**
  * Extracts e-mails from single string with e-mails list delimeted by semicolons
  *
@@ -172,7 +171,7 @@ function convertToUpperCase(str) {
  *   'info@gmail.com' => ['info@gmail.com']
  */
 function extractEmails(str) {
-  throw new Error('Not implemented');
+  return str.replace(/;/g, ', ').split();
 }
 
 /**
@@ -199,9 +198,8 @@ function extractEmails(str) {
  *
  */
 function getRectangleString(width, height) {
-  throw new Error('Not implemented');
+  return `┌${'─'.repeat(width)}┐\n` + `${` │${' '.repeat(width)}│\n`.repeat(height)}` + ` └${'─'.repeat(width)}┘\n`;
 }
-
 
 /**
  * Encode specified string with ROT13 cipher
@@ -220,7 +218,22 @@ function getRectangleString(width, height) {
  *
  */
 function encodeToRot13(str) {
-  throw new Error('Not implemented');
+  let result = '';
+  for(let i = 0; i<str.length; i++){
+    let current = str.charCodeAt(i);
+    if(current == 32 || current<65) {
+      current -=13;
+    }
+    current += 13;
+    if(current>90 && current<104) {
+      current = current - 90 + 64;
+    }
+    if(current>122) {
+      current = current - 122 + 96;
+    }
+    result += String.fromCharCode(current);
+  }
+  return result
 }
 
 /**
@@ -237,7 +250,7 @@ function encodeToRot13(str) {
  *   isString(new String('test')) => true
  */
 function isString(value) {
-  throw new Error('Not implemented');
+  return (typeof value == 'string')? true: false
 }
 
 
@@ -266,7 +279,15 @@ function isString(value) {
  *   'K♠' => 51
  */
 function getCardId(value) {
-  throw new Error('Not implemented');
+  const deck = [];
+  const types = ['♣', '♦', '♥', '♠'];
+  const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+  for(let type1 = 0; type1<types.length; type1++){
+    for(let value1 = 0; value1<values.length; value1++){
+      deck.push(values[value1] + types[type1]);
+    }
+  }
+  return deck[value];
 }
 
 module.exports = {
